adceddccb247b35cead309d7888b3837b72b0acc
JorelBaywiss <joel.bariss@student.education.wa.edu.au>
2020-05-14 14:18:15 +0800

    Added the find function

--- mtgStorage.py
+++ mtgStorage.py
@@ -1,11 +1,17 @@
 #MTG Storage.
+from os import system
+def find(a,b):
+    c=False
+    for item in a:
+        if item==b:
+            c=True
+    return c
 class card:
-    def __init__(self,name,text,setcode,rarity):
+    def __init__(self,name,manacost,text,setcode,rarity):
         self.name=name
+        self.manacost=manacost
         self.text=text
         self.setcode=setcode
         self.rarity=rarity
-    def stupid(self):
-        print(self.name)
-hi=card("me","knew","here","ok")
-hi.stupid()
+        #color
+        list_holder=[]
