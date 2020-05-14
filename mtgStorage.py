050dee80048a2f9297fcc18482c0d22ff703a452
JorelBaywiss <joel.bariss@student.education.wa.edu.au>
2020-05-14 14:11:34 +0800

    class created

--- mtgStorage.py
+++ mtgStorage.py
@@ -1,1 +1,11 @@
-#hi
+#MTG Storage.
+class card:
+    def __init__(self,name,text,setcode,rarity):
+        self.name=name
+        self.text=text
+        self.setcode=setcode
+        self.rarity=rarity
+    def stupid(self):
+        print(self.name)
+hi=card("me","knew","here","ok")
+hi.stupid()
